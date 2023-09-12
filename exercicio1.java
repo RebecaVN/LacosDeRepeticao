59603d4bad2ecca29d6c0cb44ce6eb5bd668af89
Rebeca Nunes <rebecaviananunes@gmail.com>
2023-09-12 09:54:56 -0300

    Add files via upload

---
+++ exercicio1.java
@@ -0,0 +1,32 @@
+
+public class exercicio1 {
+		//Comentário de uma linha.
+		//Método principal Main.
+		
+		public static void main(String[] args) {
+			//Primeiro laço de repetição.
+			// Impressão dos números de 1 a 10.
+			// For;
+			
+			for(int x = 1; x  < 101; x++){
+				//int = 1 -> Atribuição. 
+				// x < 11 -> Condição, ou seja, ficará no for até 10.
+				// x++ -> Incremento, ou seja, adiciona uma unidade de cada vez.
+				// x++ -> x = x+1.
+				// x-- -> Decremento.
+				// x = x + 2 -> adiciona duas unidades ao x.
+				// Imprimindo o valor de x.
+				
+				System.out.println(x);	
+			
+			}	System.out.println("-----------------");
+			
+			for(int x = 100; x > 0; x--){
+				System.out.println(x);
+			}
+		}
+}
+
+
+
+
